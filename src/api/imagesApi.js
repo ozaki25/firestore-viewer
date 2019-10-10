a542d32f11c0d8d9581b271d0c29527fed41c0f1
ozaki <fs65ugumori@yahoo.co.jp>
2019-10-10 23:18:01 +0900

    update api

--- src/api/imagesApi.js
+++ src/api/imagesApi.js
@@ -3,8 +3,8 @@ const url = `${process.env.REACT_APP_API_URL}/images`;
 async function findAll() {
   try {
     const response = await fetch(url);
-    const json = await response.json();
-    return json;
+    const images = await response.json();
+    return { images };
   } catch (e) {
     console.log(e);
     return { error: e.stack };
--- src/api/messagesApi.js
+++ src/api/messagesApi.js
@@ -3,8 +3,8 @@ const url = `${process.env.REACT_APP_API_URL}/messages`;
 async function findAll() {
   try {
     const response = await fetch(url);
-    const json = await response.json();
-    return json;
+    const messages = await response.json();
+    return { messages };
   } catch (e) {
     console.log(e);
     return { error: e.stack };
