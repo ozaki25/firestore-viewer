01d980c42332b1bd37b82cc1604f28b2695934b1
ozaki <fs65ugumori@yahoo.co.jp>
2019-10-10 22:42:48 +0900

    add images api

---
+++ src/api/imagesApi.js
@@ -0,0 +1,16 @@
+const url = `${process.env.REACT_APP_API_URL}/images`;
+
+async function findAll() {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+  } catch (e) {
+    console.log(e);
+    return { error: e.stack };
+  }
+}
+
+export default {
+  findAll,
+};
