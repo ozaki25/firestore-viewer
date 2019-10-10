e80594267a72436f3113e286d1c691cc607dde06
ozaki <fs65ugumori@yahoo.co.jp>
2019-10-10 23:27:21 +0900

    add useImages

---
+++ src/hooks/useImages.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import imagesApi from '../api/imagesapi';
+
+const initialState = {
+  images: [],
+  loading: false,
+  error: '',
+};
+
+function useMessages() {
+  const [state, setState] = useState(initialState);
+
+  const fetchMessages = async () => {
+    setState({ ...state, loading: true });
+    const { images, error } = await imagesApi.findAll();
+    setState({ images, loading: false, error });
+  };
+
+  useEffect(() => {
+    fetchMessages();
+  }, []);
+
+  return { ...state, refetch: fetchMessages };
+}
+
+export default useMessages;
