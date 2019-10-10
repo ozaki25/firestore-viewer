19bb6d2ccec510676aec1f68a8db8a9ea423b22b
ozaki <fs65ugumori@yahoo.co.jp>
2019-10-11 00:11:59 +0900

    add delete to screen

--- src/App.js
+++ src/App.js
@@ -8,21 +8,52 @@ const Image = styled.img`
 `;
 
 function App() {
-  const { messages, loading: messagesLoading } = useMessages();
-  const { images, loading: imagesLoading } = useImages();
+  const {
+    messages,
+    loading: messagesLoading,
+    destory: destoryMessage,
+  } = useMessages();
+  const { images, loading: imagesLoading, destory: destoryImage } = useImages();
+
+  const onClickDeleteMessage = id => destoryMessage(id);
+
+  const onClickDeleteImage = id => destoryImage(id);
+
   return messagesLoading || imagesLoading ? (
     <p>...loading</p>
   ) : (
     <>
       <>
         {messages.length &&
-          messages.map(message => <p key={message.id}>{message.content}</p>)}
+          messages.map(message => (
+            <p key={message.id}>
+              {message.content}
+              <a
+                href="#"
+                onClick={e => {
+                  e.preventDefault();
+                  onClickDeleteMessage(message.id);
+                }}
+              >
+                削除
+              </a>
+            </p>
+          ))}
       </>
       <>
         {images.length &&
           images.map(image => (
             <div key={image.id}>
               <Image src={image.url} alt={image.cap} />
+              <a
+                href="#"
+                onClick={e => {
+                  e.preventDefault();
+                  onClickDeleteImage(image.id);
+                }}
+              >
+                削除
+              </a>
               <p>{image.caption}</p>
               {image.comment && <p>{image.comment}</p>}
             </div>
