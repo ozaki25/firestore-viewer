1015861ec05f836f86b6c4537a517255c34c2948
ozaki <fs65ugumori@yahoo.co.jp>
2019-10-11 07:42:43 +0900

    refactor

--- src/components/Images.js
+++ src/components/Images.js
@@ -3,32 +3,33 @@ import styled from '@emotion/styled';
 import { Link } from '@chakra-ui/core';
 import useImages from '../hooks/useImages';
 
-const Image = styled.img`
+const StyledImage = styled.img`
   max-height: 100px;
 `;
+
+function Image({ image, destory }) {
+  const onClickDestory = e => {
+    e.preventDefault();
+    destory(image.id);
+  };
+
+  return (
+    <div key={image.id}>
+      <StyledImage src={image.url} alt={image.cap} />
+      <Link href="#" onClick={onClickDestory}>
+        削除
+      </Link>
+      <p>{image.caption}</p>
+      {image.comment && <p>{image.comment}</p>}
+    </div>
+  );
+}
 
 function Images() {
   const { images, loading, destory } = useImages();
 
-  const onClickDeleteImage = id => destory(id);
-
   return images.length
-    ? images.map(image => (
-        <div key={image.id}>
-          <Image src={image.url} alt={image.cap} />
-          <Link
-            href="#"
-            onClick={e => {
-              e.preventDefault();
-              onClickDeleteImage(image.id);
-            }}
-          >
-            削除
-          </Link>
-          <p>{image.caption}</p>
-          {image.comment && <p>{image.comment}</p>}
-        </div>
-      ))
+    ? images.map(image => <Image image={image} destory={destory} />)
     : null;
 }
 
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -2,26 +2,26 @@ import React from 'react';
 import useMessages from '../hooks/useMessages';
 import { Link } from '@chakra-ui/core';
 
+function Message({ message, destory }) {
+  const onClickDestory = e => {
+    e.preventDefault();
+    destory(message.id);
+  };
+  return (
+    <p key={message.id}>
+      {message.content}
+      <Link href="#" onClick={onClickDestory}>
+        削除
+      </Link>
+    </p>
+  );
+}
+
 function Messages() {
   const { messages, loading, destory } = useMessages();
 
-  const onClickDeleteMessage = id => destory(id);
-
   return messages.length
-    ? messages.map(message => (
-        <p key={message.id}>
-          {message.content}
-          <Link
-            href="#"
-            onClick={e => {
-              e.preventDefault();
-              onClickDeleteMessage(message.id);
-            }}
-          >
-            削除
-          </Link>
-        </p>
-      ))
+    ? messages.map(message => <Message message={message} destory={destory} />)
     : null;
 }
 
