043b70920cd82c2dd4c9d7026d7392cf685b5dcd
ozaki <fs65ugumori@yahoo.co.jp>
2019-10-10 23:30:39 +0900

    show index

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 import useMessages from './hooks/useMessages';
+import useImages from './hooks/useImages';
+
+const Image = styled.img`
+  max-height: 100px;
+`;
 
 function App() {
-  const { messages, loading } = useMessages();
-  return loading ? (
+  const { messages, loading: messagesLoading } = useMessages();
+  const { images, loading: imagesLoading } = useImages();
+  return messagesLoading || imagesLoading ? (
     <p>...loading</p>
   ) : (
-    messages.map(message => (
-      <p key={message.timestamp._nanoseconds}>{message.content}</p>
-    ))
+    <>
+      <>
+        {messages.length &&
+          messages.map(message => (
+            <p key={message.timestamp._nanoseconds}>{message.content}</p>
+          ))}
+      </>
+      <>
+        {images.length &&
+          images.map(image => (
+            <div key={image.timestamp._nanoseconds}>
+              <Image src={image.url} alt={image.cap} />
+              <p>{image.caption}</p>
+              {image.comment && <p>{image.comment}</p>}
+            </div>
+          ))}
+      </>
+    </>
   );
 }
 
