99495542ab3b0bbc6bf7affa261b6b5e3ccdd391
ozaki <fs65ugumori@yahoo.co.jp>
2019-10-10 23:18:14 +0900

    update hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import useMessages from './hooks/useMessages';
 
 function App() {
-  return <p>Hello</p>;
+  const { messages, loading } = useMessages();
+  return loading ? (
+    <p>...loading</p>
+  ) : (
+    messages.map(message => (
+      <p key={message.timestamp._nanoseconds}>{message.content}</p>
+    ))
+  );
 }
 
 export default App;
---
+++ src/hooks/useMessages.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import messagesApi from '../api/messagesApi';
+
+const initialState = {
+  messages: [],
+  loading: false,
+  error: '',
+};
+
+function useMessages() {
+  const [state, setState] = useState(initialState);
+
+  const fetchMessages = async () => {
+    setState({ ...state, loading: true });
+    const { messages, error } = await messagesApi.findAll();
+    setState({ messages, loading: false, error });
+  };
+
+  useEffect(() => {
+    fetchMessages();
+  }, []);
+
+  return { ...state, refetch: fetchMessages };
+}
+
+export default useMessages;
